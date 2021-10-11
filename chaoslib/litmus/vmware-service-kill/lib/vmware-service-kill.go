9ee2b11bd8f31adddebe8ad3da6386315e7e424c
neelanjan00 <neelanjan@chaosnative.com>
2021-10-11 21:28:34 +0530

    refactored serial and parallel chaos injection files into two separate functions
    Signed-off-by: neelanjan00 <neelanjan@chaosnative.com>

--- chaoslib/litmus/vmware-service-kill/lib/vmware-service-kill.go
+++ chaoslib/litmus/vmware-service-kill/lib/vmware-service-kill.go
@@ -5,22 +5,22 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/chaosnative/litmus-go/pkg/cloud/vmware"
 	experimentTypes "github.com/chaosnative/litmus-go/pkg/vmware/vmware-service-kill/types"
 	"github.com/litmuschaos/litmus-go/pkg/clients"
+	"github.com/litmuschaos/litmus-go/pkg/events"
 	"github.com/litmuschaos/litmus-go/pkg/log"
+	"github.com/litmuschaos/litmus-go/pkg/probe"
 	"github.com/litmuschaos/litmus-go/pkg/types"
 	"github.com/litmuschaos/litmus-go/pkg/utils/common"
 	"github.com/pkg/errors"
-
-	serviceKillAndCheckModes "github.com/chaosnative/litmus-go/chaoslib/litmus/vmware-service-kill/lib/service-kill-and-check/lib"
-	serviceKillAndRestartModes "github.com/chaosnative/litmus-go/chaoslib/litmus/vmware-service-kill/lib/service-kill-and-restart/lib"
 )
 
 var inject, abort chan os.Signal
 
-// PrepareServiceKill facilitates the prepration and injection steps for the experiment as per the value of SelfHealingServices
+// PrepareServiceKill facilitates chaos prepration and injection as per the value of SelfHealingServices
 func PrepareServiceKill(experimentsDetails *experimentTypes.ExperimentDetails, clients clients.ClientSets, resultDetails *types.ResultDetails, eventsDetails *types.EventDetails, chaosDetails *types.ChaosDetails) error {
 
 	switch experimentsDetails.SelfHealingServices {
@@ -39,6 +39,7 @@ func PrepareServiceKill(experimentsDetails *experimentTypes.ExperimentDetails, c
 	return nil
 }
 
+// serviceKillAndCheck executes chaos prepration and injection steps for self-healing services
 func serviceKillAndCheck(experimentsDetails *experimentTypes.ExperimentDetails, clients clients.ClientSets, resultDetails *types.ResultDetails, eventsDetails *types.EventDetails, chaosDetails *types.ChaosDetails) error {
 
 	//Waiting for the ramp time before chaos injection
@@ -55,11 +56,11 @@ func serviceKillAndCheck(experimentsDetails *experimentTypes.ExperimentDetails, 
 
 	switch strings.ToLower(experimentsDetails.Sequence) {
 	case "serial":
-		if err := serviceKillAndCheckModes.ServiceKillAndCheckSerialMode(experimentsDetails, serviceNamesList, clients, resultDetails, eventsDetails, chaosDetails); err != nil {
+		if err := injectChaosInSerialMode(experimentsDetails, serviceNamesList, clients, resultDetails, eventsDetails, chaosDetails); err != nil {
 			return err
 		}
 	case "parallel":
-		if err := serviceKillAndCheckModes.ServiceKillAndCheckParallelMode(experimentsDetails, serviceNamesList, clients, resultDetails, eventsDetails, chaosDetails); err != nil {
+		if err := injectChaosInParallelMode(experimentsDetails, serviceNamesList, clients, resultDetails, eventsDetails, chaosDetails); err != nil {
 			return err
 		}
 	default:
@@ -75,6 +76,7 @@ func serviceKillAndCheck(experimentsDetails *experimentTypes.ExperimentDetails, 
 	return nil
 }
 
+// serviceKillAndRestart executes chaos prepration and injection steps for non self-healing services
 func serviceKillAndRestart(experimentsDetails *experimentTypes.ExperimentDetails, clients clients.ClientSets, resultDetails *types.ResultDetails, eventsDetails *types.EventDetails, chaosDetails *types.ChaosDetails) error {
 
 	// inject channel is used to transmit signal notifications.
@@ -110,11 +112,11 @@ func serviceKillAndRestart(experimentsDetails *experimentTypes.ExperimentDetails
 
 		switch strings.ToLower(experimentsDetails.Sequence) {
 		case "serial":
-			if err := serviceKillAndRestartModes.ServiceKillAndRestartSerialMode(experimentsDetails, serviceNamesList, clients, resultDetails, eventsDetails, chaosDetails); err != nil {
+			if err := injectChaosInSerialMode(experimentsDetails, serviceNamesList, clients, resultDetails, eventsDetails, chaosDetails); err != nil {
 				return err
 			}
 		case "parallel":
-			if err := serviceKillAndRestartModes.ServiceKillAndRestartParallelMode(experimentsDetails, serviceNamesList, clients, resultDetails, eventsDetails, chaosDetails); err != nil {
+			if err := injectChaosInParallelMode(experimentsDetails, serviceNamesList, clients, resultDetails, eventsDetails, chaosDetails); err != nil {
 				return err
 			}
 		default:
@@ -126,6 +128,182 @@ func serviceKillAndRestart(experimentsDetails *experimentTypes.ExperimentDetails
 			log.Infof("[Ramp]: Waiting for the %vs ramp time after injecting chaos", experimentsDetails.RampTime)
 			common.WaitForDuration(experimentsDetails.RampTime)
 		}
+	}
+
+	return nil
+}
+
+// injectChaosInSerialMode stops the services in serial mode i.e. one by one
+func injectChaosInSerialMode(experimentsDetails *experimentTypes.ExperimentDetails, serviceNamesList []string, clients clients.ClientSets, resultDetails *types.ResultDetails, eventsDetails *types.EventDetails, chaosDetails *types.ChaosDetails) error {
+	//ChaosStartTimeStamp contains the start timestamp, when the chaos injection begin
+	ChaosStartTimeStamp := time.Now()
+	duration := int(time.Since(ChaosStartTimeStamp).Seconds())
+
+	for duration < experimentsDetails.ChaosDuration {
+
+		if experimentsDetails.EngineName != "" {
+			msg := "Injecting " + experimentsDetails.ExperimentName + " chaos on service"
+			types.SetEngineEventAttributes(eventsDetails, types.ChaosInject, msg, "Normal", chaosDetails)
+			events.GenerateEvents(eventsDetails, clients, chaosDetails, "ChaosEngine")
+		}
+
+		for i, serviceName := range serviceNamesList {
+
+			//Stopping the service
+			log.Infof("[Chaos]: Stopping %s service", serviceName)
+			if err := vmware.StopService(serviceName, experimentsDetails.VMName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword); err != nil {
+				return errors.Errorf("unable to stop %s service, %s", serviceName, err)
+			}
+
+			common.SetTargets(serviceName, "injected", "Service", chaosDetails)
+
+			//Wait for service to stop
+			log.Infof("[Wait]: Wait for %s service to stop", serviceName)
+			if err := vmware.WaitForServiceStop(experimentsDetails.VcenterServer, experimentsDetails.VMName, serviceName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword, experimentsDetails.Delay, experimentsDetails.Timeout); err != nil {
+				return errors.Errorf("%s service failed to stop, %s", serviceName, err)
+			}
+
+			// run the probes during chaos
+			if len(resultDetails.ProbeDetails) != 0 && i == 0 {
+				if err := probe.RunProbes(chaosDetails, clients, resultDetails, "DuringChaos", eventsDetails); err != nil {
+					return err
+				}
+			}
+
+			if experimentsDetails.SelfHealingServices == "true" {
+
+				//Wait for service to start
+				log.Infof("[Wait]: Wait for %s service to start", serviceName)
+				if err := vmware.WaitForServiceStart(experimentsDetails.VcenterServer, experimentsDetails.VMName, serviceName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword, experimentsDetails.Delay, experimentsDetails.Timeout); err != nil {
+					return errors.Errorf("%s service failed to stop, %s", serviceName, err)
+				}
+			} else {
+
+				//Wait for chaos duration
+				log.Infof("[Wait]: Waiting for the chaos interval of %vs", experimentsDetails.ChaosInterval)
+				common.WaitForDuration(experimentsDetails.ChaosInterval)
+
+				// Get service state
+				serviceState, err := vmware.GetServiceState(serviceName, experimentsDetails.VMName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword)
+				if err != nil {
+					return errors.Errorf("failed to get %s service state, %s", serviceState, err)
+				}
+
+				switch serviceState {
+				case "active":
+					log.Infof("[Skip]: %s service is already active", serviceName)
+				default:
+					// Start the service
+					log.Info("[Chaos]: Starting %s service")
+					if err := vmware.StartService(serviceName, experimentsDetails.VMName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword); err != nil {
+						return errors.Errorf("unable to start %s service, %s", serviceName, err)
+					}
+
+					//Wait for service to start
+					log.Infof("[Wait]: Wait for %s service to start")
+					if err := vmware.WaitForServiceStop(experimentsDetails.VcenterServer, experimentsDetails.VMName, serviceName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword, experimentsDetails.Delay, experimentsDetails.Timeout); err != nil {
+						return errors.Errorf("unable to start %s service, %s", serviceName, err)
+					}
+				}
+			}
+
+			common.SetTargets(serviceName, "reverted", "Service", chaosDetails)
+		}
+
+		duration = int(time.Since(ChaosStartTimeStamp).Seconds())
+	}
+
+	return nil
+}
+
+// injectChaosInParallelMode stops the services in parallel mode i.e. all at once
+func injectChaosInParallelMode(experimentsDetails *experimentTypes.ExperimentDetails, serviceNamesList []string, clients clients.ClientSets, resultDetails *types.ResultDetails, eventsDetails *types.EventDetails, chaosDetails *types.ChaosDetails) error {
+
+	//ChaosStartTimeStamp contains the start timestamp, when the chaos injection begin
+	ChaosStartTimeStamp := time.Now()
+	duration := int(time.Since(ChaosStartTimeStamp).Seconds())
+
+	for duration < experimentsDetails.ChaosDuration {
+
+		if experimentsDetails.EngineName != "" {
+			msg := "Injecting " + experimentsDetails.ExperimentName + " chaos on service"
+			types.SetEngineEventAttributes(eventsDetails, types.ChaosInject, msg, "Normal", chaosDetails)
+			events.GenerateEvents(eventsDetails, clients, chaosDetails, "ChaosEngine")
+		}
+
+		for _, serviceName := range serviceNamesList {
+
+			//Stopping the service
+			log.Infof("[Chaos]: Stopping %s service", serviceName)
+			if err := vmware.StopService(serviceName, experimentsDetails.VMName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword); err != nil {
+				return errors.Errorf("unable to stop %s service, %s", serviceName, err)
+			}
+
+			common.SetTargets(serviceName, "injected", "Service", chaosDetails)
+		}
+
+		for _, serviceName := range serviceNamesList {
+
+			//Wait for service to stop
+			log.Infof("[Wait]: Wait for %s service to stop", serviceName)
+			if err := vmware.WaitForServiceStop(experimentsDetails.VcenterServer, experimentsDetails.VMName, serviceName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword, experimentsDetails.Delay, experimentsDetails.Timeout); err != nil {
+				return errors.Errorf("%s service failed to stop, %s", serviceName, err)
+			}
+		}
+
+		// run the probes during chaos
+		if len(resultDetails.ProbeDetails) != 0 {
+			if err := probe.RunProbes(chaosDetails, clients, resultDetails, "DuringChaos", eventsDetails); err != nil {
+				return err
+			}
+		}
+
+		if experimentsDetails.SelfHealingServices == "true" {
+			for _, serviceName := range serviceNamesList {
+
+				//Wait for service to start
+				log.Infof("[Wait]: Wait for %s service to start", serviceName)
+				if err := vmware.WaitForServiceStart(experimentsDetails.VcenterServer, experimentsDetails.VMName, serviceName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword, experimentsDetails.Delay, experimentsDetails.Timeout); err != nil {
+					return errors.Errorf("%s service failed to stop, %s", serviceName, err)
+				}
+
+				common.SetTargets(serviceName, "reverted", "Service", chaosDetails)
+			}
+		} else {
+			//Wait for chaos interval
+			log.Infof("[Wait]: Waiting for the chaos interval of %vs", experimentsDetails.ChaosInterval)
+			common.WaitForDuration(experimentsDetails.ChaosInterval)
+
+			for _, serviceName := range serviceNamesList {
+
+				// Get the service state
+				serviceState, err := vmware.GetServiceState(serviceName, experimentsDetails.VMName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword)
+				if err != nil {
+					return errors.Errorf("failed to get %s service state, %s", serviceState, err)
+				}
+
+				switch serviceState {
+				case "active":
+					log.Infof("[Skip]: %s service is already active", serviceName)
+				default:
+					// Start the service
+					log.Info("[Chaos]: Starting %s service")
+					if err := vmware.StartService(serviceName, experimentsDetails.VMName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword); err != nil {
+						return errors.Errorf("unable to start %s service, %s", serviceName, err)
+					}
+
+					//Wait for service to start
+					log.Infof("[Wait]: Wait for %s service to start")
+					if err := vmware.WaitForServiceStop(experimentsDetails.VcenterServer, experimentsDetails.VMName, serviceName, experimentsDetails.Datacenter, experimentsDetails.VMUserName, experimentsDetails.VMPassword, experimentsDetails.Delay, experimentsDetails.Timeout); err != nil {
+						return errors.Errorf("unable to start %s service, %s", serviceName, err)
+					}
+				}
+
+				common.SetTargets(serviceName, "reverted", "Service", chaosDetails)
+			}
+		}
+
+		duration = int(time.Since(ChaosStartTimeStamp).Seconds())
 	}
 
 	return nil
