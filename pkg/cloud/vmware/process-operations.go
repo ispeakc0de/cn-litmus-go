4e12423868f67c8e9af2c702e160d52702f2cd93
neelanjan00 <neelanjan@chaosnative.com>
2021-10-21 10:01:32 +0530

    added kill process function
    Signed-off-by: neelanjan00 <neelanjan@chaosnative.com>

--- pkg/cloud/vmware/process-operations.go
+++ pkg/cloud/vmware/process-operations.go
@@ -1,1 +1,20 @@
 package vmware
+
+import (
+	"fmt"
+
+	"github.com/chaosnative/litmus-go/pkg/utils"
+)
+
+// KillLinuxProcess kills a given process in a Linux VM
+func KillLinuxProcess(pid, vmName, vmUserName, vmPassword string) error {
+
+	command := fmt.Sprintf(`govc guest.run -vm=%s -l=%s:%s "echo %s | sudo -S kill %s"`, vmName, vmUserName, vmPassword, vmPassword, pid)
+
+	_, _, err := utils.Shellout(command)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
