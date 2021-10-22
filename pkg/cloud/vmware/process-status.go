3728d1aca4e2153ffd58924836cfc5debbf48597
neelanjan00 <neelanjan@chaosnative.com>
2021-10-22 11:20:18 +0530

    added ProcessStateCheck function
    Signed-off-by: neelanjan00 <neelanjan@chaosnative.com>

--- pkg/cloud/vmware/process-status.go
+++ pkg/cloud/vmware/process-status.go
@@ -3,6 +3,7 @@ package vmware
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/chaosnative/litmus-go/pkg/utils"
 	"github.com/pkg/errors"
@@ -37,4 +38,56 @@ func GetProcess(processId, vmName, vmUserName, vmPassword string) (bool, error) 
 	}
 
 	return true, nil
+}
+
+// ProcessStateCheck validates that all the given processes are alive
+func ProcessStateCheck(vmName, processIds, vmUserName, vmPassword string) error {
+
+	if vmName == "" {
+		return errors.Errorf("no vm name provided")
+	}
+
+	if err := GetVM(vmName); err != nil {
+		return errors.Errorf("failed to get vm %s, err: %s", vmName, err.Error())
+	}
+
+	if vmUserName == "" {
+		return errors.Errorf("no vm username provided")
+	}
+
+	if vmPassword == "" {
+		return errors.Errorf("no vm password provided")
+	}
+
+	connectionState, powerState, err := GetVMState(vmName)
+	if err != nil {
+		return errors.Errorf("unable to fetch vm %s state, %s", vmName, err.Error())
+	}
+
+	if connectionState != "connected" {
+		return errors.Errorf("vm %s is not in connected state", vmName)
+	}
+
+	if powerState != "poweredOn" {
+		return errors.Errorf("vm %s is not in powered-on state", vmName)
+	}
+
+	processIdList := strings.Split(processIds, ",")
+	if len(processIdList) == 0 {
+		return errors.Errorf("no process id provided")
+	}
+
+	for _, processId := range processIdList {
+
+		isProcessAlive, err := GetProcess(processId, vmName, vmUserName, vmPassword)
+		if err != nil {
+			return errors.Errorf("failed to get process %s, err: %s", processId, err.Error())
+		}
+
+		if !isProcessAlive {
+			return errors.Errorf("process %s not found", processId)
+		}
+	}
+
+	return nil
 }
